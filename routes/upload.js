600f04931d74dd06fba4d7434ae4f088272fc2ad
chao.zhang <chao.zhang@beibei.com>
2020-08-20 14:42:58 +0800

    fix 接口命名

--- routes/upload.js
+++ routes/upload.js
@@ -1,13 +1,12 @@
-const router = require('koa-router')()
-const fs = require('fs');
+const router = require('koa-router')();
 const path = require('path');
 const fse = require('fs-extra');
-const { UPLOAD_DIR, TEMP_DIR } = require('../shared/constants');
-const uploadController = require('../controller/upload');
+const {UPLOAD_DIR, TEMP_DIR} = require('../shared/constants');
+const UploadController = require('../controller/upload');
 
-const uploadCtrl = new uploadController();
+const uploadCtrl = new UploadController();
 // 单文件文件上传（小文件）
-router.post('/upload', async ctx => {
+router.post('/upload', async (ctx) => {
     const {
         materialType,
         folderName,
@@ -15,10 +14,10 @@ router.post('/upload', async ctx => {
     const filePath = `${UPLOAD_DIR}/${materialType}/${folderName}`;
     let res = await uploadCtrl.uploadStatic(ctx, filePath);
     ctx.body = res;
-})
+});
 
 // 多文件文件上传（小文件）
-router.post('/upload-duo', async ctx => {
+router.post('/upload/batch', async (ctx) => {
     const {
         materialType,
         folderName,
@@ -26,47 +25,41 @@ router.post('/upload-duo', async ctx => {
     const filePath = `${UPLOAD_DIR}/${materialType}/${folderName}`;
     let res = await uploadCtrl.uploadStatics(ctx, filePath);
     ctx.body = res;
-})
+});
 
 // 单文件文件分片上传（大文件）
-router.post('/fen-upload', async ctx => {
-    const {
-        uid,
-        materialType,
-        folderName,
-        fileHash,
-    } = ctx.request.body
-    const file = ctx.request.files.file
+router.post('/upload/shard', async (ctx) => {
+    const {fileHash} = ctx.request.body;
+    const file = ctx.request.files.file;
 
     // 存放切片的临时目录
     const chunkDir = `${TEMP_DIR}/${fileHash}`;
     if (!fse.existsSync(chunkDir)) { // 没有目录就创建目录
         // 创建大文件的临时目录
-        await fse.mkdirs(chunkDir)
+        await fse.mkdirs(chunkDir);
     }
     // 原文件名.index - 每个分片的具体地址和名字
-    const dPath = path.join(chunkDir, file.name)
+    const dPath = path.join(chunkDir, file.name);
 
     // 将分片文件从 temp 中移动到本次上传大文件的临时目录
     await fse.move(file.path, dPath, {
-        overwrite: true
-    })
-    ctx.body = '文件上传成功'
-})
+        overwrite: true,
+    });
+    ctx.body = '文件上传成功';
+});
 
 // 合并文件
-router.post('/merge', async ctx => {
+router.post('/merge', async (ctx) => {
     const {
         name,
-        uid,
         materialType,
         folderName,
         fileHash,
-    } = ctx.request.body
+    } = ctx.request.body;
     const fname = `${fileHash}`;
 
-    const chunkDir = path.join(TEMP_DIR, fname)
-    const chunks = await fse.readdir(chunkDir)
+    const chunkDir = path.join(TEMP_DIR, fname);
+    const chunks = await fse.readdir(chunkDir);
 
     // 创建当前文件存储真实位置
     const currentDir = `${UPLOAD_DIR}/${materialType}/${folderName}`;
@@ -74,30 +67,30 @@ router.post('/merge', async ctx => {
         fse.mkdirsSync(currentDir);
     }
 
-    chunks.sort((a, b) => a - b).map(chunkPath => {
+    chunks.sort((aa, bb) => aa - bb).map((chunkPath) => { // eslint-disable-line
         // 合并文件
         fse.appendFileSync(
             path.join(currentDir, name),
             fse.readFileSync(`${chunkDir}/${chunkPath}`)
-        )
-    })
+        );
+    });
     // 删除临时文件夹
-    fse.removeSync(chunkDir)
+    fse.removeSync(chunkDir);
     // 返回文件地址
     ctx.body = {
         msg: '合并成功',
-        url: `http://localhost:3000/upload/${name}`
-    }
-})
+        url: `http://localhost:3000/upload/${name}`,
+    };
+});
 
-router.post('/mkdir', async ctx => {
-    const { file_name } = ctx.request.body;
+// eslint-disable-next-line
+router.post('/mkdir', async (ctx) => {
+    const {file_name} = ctx.request.body;
     const baseUrl = `${UPLOAD_DIR}/${file_name}/公共`;
     if (!fse.existsSync(baseUrl)) {
         fse.mkdirs(baseUrl);
-        console.log('file_name: ', file_name);
     }
     ctx.body = 'success';
-})
+});
 
 module.exports = router;
--- routes/verify.js
+++ routes/verify.js
@@ -1,30 +1,29 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 const path = require('path');
 const fse = require('fs-extra');
-const { UPLOAD_DIR, TEMP_DIR } = require('../shared/constants');
-const { getUploadedList } = require('../shared/util');
+const {UPLOAD_DIR, TEMP_DIR} = require('../shared/constants');
+const {getUploadedList} = require('../shared/util');
 
-router.post('/verify', async ctx => {
+router.post('/verify', async (ctx) => {
     const {
         name,
         materialType,
         folderName,
-        uid,
         fileHash,
-    } = ctx.request.body
+    } = ctx.request.body;
     const fileDir = `${UPLOAD_DIR}/${materialType}/${folderName}/${name}`;
     // 文件是否存在
-    let uploaded = false
-    let uploadedList = []
+    let uploaded = false;
+    let uploadedList = [];
     if (fse.existsSync(fileDir)) {
         uploaded = true;
     } else {
-        uploadedList = await getUploadedList(path.resolve(TEMP_DIR, fileHash))
+        uploadedList = await getUploadedList(path.resolve(TEMP_DIR, fileHash));
     }
     ctx.body = {
         uploaded,
         uploadedList,
     };
-})
+});
 
 module.exports = router;
